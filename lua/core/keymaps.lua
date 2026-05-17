40755bbf9304a322ca1519ca29ef64bb2fe11167
LoftyReign <189904080+LoftyReign@users.noreply.github.com>
2026-05-17 16:18:53 +0200

    Updated diagnostics api

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,20 +4,25 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
 
-vim.keymap.set(
-	"n",
-	"<C-j>",
-	"<cmd>lua vim.diagnostic.goto_next({float={source=true}})<cr>",
-	{ desc = "Next diagnostic" }
-)
-vim.keymap.set(
-	"n",
-	"<C-k>",
-	"<cmd>lua vim.diagnostic.goto_prev({float={source=true}})<cr>",
-	{ desc = "Prev diagnostic" }
-)
-vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Toggle local troubleshoot" })
+vim.keymap.set("n", "<C-j>", function()
+	vim.diagnostic.jump({
+		count = 1,
+		on_jump = function()
+			vim.diagnostic.open_float({ source = true })
+		end,
+	})
+end, { desc = "Next diagnostic" })
 
+vim.keymap.set("n", "<C-k>", function()
+	vim.diagnostic.jump({
+		count = -1,
+		on_jump = function()
+			vim.diagnostic.open_float({ source = true })
+		end,
+	})
+end, { desc = "Prev diagnostic" })
+
+vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Toggle local troubleshoot" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
 
 -- Do not copy deletions into clipboard
